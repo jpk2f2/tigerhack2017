9d5772d242a6d0e2deefbf9a44190cc4c4896ef4
David Emily <daeth5@mail.missouri.edu>
2017-10-14 12:22:56 -0500

    score image looking for picture in github repo

--- scoreImage.py
+++ scoreImage.py
@@ -11,7 +11,8 @@ params = urllib.parse.urlencode({
 })
 
 # Replace the example URL below with the URL of the image you want to analyze.
-body = "{ 'url': 'https://blog.caranddriver.com/wp-content/uploads/2009/07/2010-Ferrari-458-Italia-1011-626x382.jpg' }"
+#body = "{ 'url': 'https://raw.githubusercontent.com/davidemily/testPics/master/photo.bmp' }"
+body = "{ 'url': 'http://craftycat3.ddns.net/photos/download.jpg' }"
 
 try:
     # NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
@@ -19,7 +20,7 @@ try:
     #   URL below with "westcentralus".
     conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
     conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
-    response = conn.getresponse():
+    response = conn.getresponse()
     data = response.read()
     print(data)
     conn.close()
