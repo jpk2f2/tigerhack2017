0103da4586e930c7486d598f43b3f5ebf989280b
David Emily <daeth5@mail.missouri.edu>
2017-10-14 14:02:58 -0500

    pushes image to testpics dir

--- pygameImage.py
+++ pygameImage.py
@@ -1,8 +1,19 @@
+import os
 import pygame.camera
+import time
 pygame.camera.init()
 cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
 cam.start()
 img = cam.get_image()
 import pygame.image
 pygame.image.save(img, "../testPic/photo.bmp")
+os.chdir('/home/david/testPic')
+time.sleep(2)
+os.system('git init')
+time.sleep(2)
+os.system('git add photo.bmp')
+time.sleep(2)
+os.system('git commit -m "new image"')
+time.sleep(2)
+os.system('git push origin master')
 pygame.camera.quit()
