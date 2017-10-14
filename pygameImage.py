c9e4a76d8b48c87300ca53ecc6022001dcf76054
David Emily <daeth5@mail.missouri.edu>
2017-10-14 12:55:40 -0500

    before I fuck this up

--- pygameImage.py
+++ pygameImage.py
@@ -4,5 +4,5 @@ cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
 cam.start()
 img = cam.get_image()
 import pygame.image
-pygame.image.save(img, "photo.bmp")
+pygame.image.save(img, "../testPic/photo.bmp")
 pygame.camera.quit()
